80e524d427bb20f1e04a71a76884bb3ee5070074
Jamie Patel <jamiepatel@Jamies-MacBook-Pro.local>
2014-06-20 00:36:27 +0100

    Reinitialize

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://www.rubygems.org'
+
+ruby '2.1.1'
+
+gem 'sinatra'
+gem 'nokogiri'
---
+++ app.rb
@@ -0,0 +1,40 @@
+require 'nokogiri'
+require 'open-uri'
+require 'sinatra'
+
+# enable :sessions
+
+get '/' do 
+	main_page = Nokogiri::HTML(open('http://www.bbc.co.uk/sport/0/football/'))
+
+	main_page.css('span.status.live').each do |status|
+		@doc = "http://www.bbc.co.uk" + status.parent.parent.attr('href')
+	end
+
+	@match = Nokogiri::HTML(open(@doc))
+
+	def instruction
+		@match.css('.event').each do |event|
+			if event.content.include?("Goal" || "goal")
+				return "Down four shots: #{event.content}"
+			elsif event.content.include?("Red card" || "red card" || "sent off" || "sending off" || "Dismissal" || "dismissal" || "second yellow card" || "Second yellow card")
+				return "Down three shots: #{event.content}"
+			elsif event.content.include?("Substitution" || "substitution" || "replaces")
+				return "Down a shot: #{event.content}"
+			elsif event.content.include?("yellow card" || "Booking" || "booking")
+				return "Down a shot: #{event.content}"
+			end
+		end
+	end
+	
+	@instruction = instruction
+	erb :index
+end
+
+# # puts event
+
+# get '/' do 
+# 	erb :index
+# end
+
+# puts instruction
